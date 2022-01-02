d0abfdc00d34b5472a3c1ce4f5384c081443d0a5
Christopher Donnelly <cad605@nyu.edu>
2022-01-01 20:16:45 -0500

    init database

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,28 @@
+import type {LinksFunction} from 'remix'
+import {Link} from 'remix'
+import stylesUrl from '~/styles/index.css'
+
+export const links: LinksFunction = () => {
+  return [{rel: 'stylesheet', href: stylesUrl}]
+}
+
 // The app/routes/index.tsx is a child of the app/root.tsx route.
 // In nested routing, parents are responsible for laying out their children.
-export default function IndexRoute() {
-  return <div>Hello Index Route!</div>
+export default function Index() {
+  return (
+    <div className="container">
+      <div className="content">
+        <h1>
+          Remix <span>Jokes!</span>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="jokes">Read Jokes</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  )
 }
